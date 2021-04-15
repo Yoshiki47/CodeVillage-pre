e2404ef4784ac7af0be6ee09827db717a4f00dec
yoshiki <katoyoshi9814@gmail.com>
2021-04-15 12:39:27 +0900

    スムーズスクロール改善

--- yoshiki47.pre/js/main.js
+++ yoshiki47.pre/js/main.js
@@ -1,51 +1,51 @@
 // 各idにクラスを追加
 function ham() {
-    document.getElementById('top-span').classList.toggle('active1');
-    document.getElementById('center-span').classList.toggle('active2');
-    document.getElementById('bottom-span').classList.toggle('active3');
-    document.getElementById('ham-nav-menu').classList.toggle('in');
-    document.getElementById('ham').classList.toggle('ham-fixed');
+	document.getElementById('top-span').classList.toggle('active1');
+	document.getElementById('center-span').classList.toggle('active2');
+	document.getElementById('bottom-span').classList.toggle('active3');
+	document.getElementById('ham-nav-menu').classList.toggle('in');
+	document.getElementById('ham').classList.toggle('ham-fixed');
 }
 
 // ハンバーガーメニュークリック時にクラス追加
 document.getElementById('ham').addEventListener('click', function() {
-    ham();
+	ham();
 });
 
 // 各idからクラスを削除
 function removeClass() {
-    document.getElementById('top-span').classList.remove('active1');
-    document.getElementById('center-span').classList.remove('active2');
-    document.getElementById('bottom-span').classList.remove('active3');
-    document.getElementById('ham-nav-menu').classList.remove('in');
+	document.getElementById('top-span').classList.remove('active1');
+	document.getElementById('center-span').classList.remove('active2');
+	document.getElementById('bottom-span').classList.remove('active3');
+	document.getElementById('ham-nav-menu').classList.remove('in');
 }
 
 // id="ham"からham-fixedクラスを削除
 function removeFixed() {
-    document.getElementById('ham').classList.remove('ham-fixed');
+	document.getElementById('ham').classList.remove('ham-fixed');
 }
 
-// ハンバーがアーメニュークリック時に追加されたclassを画面クリック時に削除
+// ハンバーガーメニュークリック時に追加されたclassを画面クリック時に削除
 document.getElementById('ham-nav-menu').addEventListener('click', function() {
-    removeClass();
-    removeFixed();
+	removeClass();
+	removeFixed();
 });
 
-
 // スムーズスクロール
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < smoothScrollTrigger.length; i++) {
-    smoothScrollTrigger[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        let href = smoothScrollTrigger[i].getAttribute('href');
-        let targetElement = document.getElementById(href.replace('#', ''));
-        const rect = targetElement.getBoundingClientRect().top;
-        const offset = window.pageYOffset;
-        const gap = 0
-        const target = rect + offset - gap;
-        window.scrollTo( {
-            top: target,
-            behavior: 'smooth',
-        });
-    });
-}
+window.addEventListener('DOMContentLoaded', () => {
+	const anchorLinks = document.querySelectorAll('a[href^="#"]');
+	const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+
+	anchorLinksArr.forEach((link) => {
+		link.addEventListener('click', (e) => {
+			e.preventDefault();
+			const targetId = link.hash;
+			const targetElement = document.querySelector(targetId);
+			const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+			window.scrollTo({
+				top: targetOffsetTop,
+				behavior: 'smooth'
+			});
+		});
+	});
+});
